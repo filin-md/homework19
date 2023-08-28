a0daca83a2d5cd31329286d4670c6a65cf2c358f
md <filin.md9@gmail.com>
2023-08-29 00:25:11 +0300

    add task3

--- blog/models.py
+++ blog/models.py
@@ -8,7 +8,7 @@ NULLABLE = {'null': True, 'blank': True}
 
 class Post(models.Model):
     title = models.CharField(max_length=100, verbose_name='Загловок')
-    slug = models.CharField(max_length=100, verbose_name='ЧПУ')
+    slug = models.CharField(max_length=100, verbose_name='ЧПУ', **NULLABLE)
     content = models.TextField(verbose_name='Текст')
     image = models.ImageField(upload_to='blog/', verbose_name='Изображение', **NULLABLE)
     create_date = models.DateTimeField(default=timezone.now, verbose_name='Дата создания')
