d0f4c9c375b8a4e11090c26ac342bf5bb5517b52
md <filin.md9@gmail.com>
2023-09-11 23:34:37 +0300

    add task3

--- catalog/models.py
+++ catalog/models.py
@@ -40,6 +40,19 @@ class Product(models.Model):
         ordering = ('price',)
 
 
+class Version(models.Model):
+    product = models.ForeignKey('Product', on_delete=models.CASCADE, verbose_name='продукт',)
+    version_number = models.PositiveSmallIntegerField(verbose_name='номер версии')
+    version_name = models.CharField(max_length=100, verbose_name='название версии')
+    version_sign = models.TextField(verbose_name='признаки версии')
+
+    def __str__(self):
+        return f'{self.version_number} - {self.version_name}'
+
+    class Meta:
+        verbose_name = 'Версия'
+        verbose_name_plural = 'версии'
+
 
 
 
--- catalog/urls.py
+++ catalog/urls.py
@@ -1,9 +1,16 @@
 from django.urls import path
 
-from catalog.views import contacts, ProductListView, ProductDetailView
+from catalog.views import contacts, ProductListView, ProductDetailView, ProductCreateView, ProductUpdateView, \
+    ProductDeleteView
+
+# app_name = 'catalog'
 
 urlpatterns = [
     path('', ProductListView.as_view(), name='home'),
     path('contacts/', contacts, name='contacts'),
-    path('product/<int:pk>/', ProductDetailView.as_view(), name='product')
+    path('product/<int:pk>/', ProductDetailView.as_view(), name='product'),
+    path('create/', ProductCreateView.as_view(), name='create_product'),
+    path('update/<int:pk>/', ProductUpdateView.as_view(), name='update_product'),
+    path('delete/<int:pk>/', ProductDeleteView.as_view(), name='delete_product'),
+
 ]
