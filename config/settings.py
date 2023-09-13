00490921650bf37d50261ee5310fb71dcbdf52b1
md <filin.md9@gmail.com>
2023-09-13 23:28:32 +0300

    22.2 homework

--- config/settings.py
+++ config/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     'catalog',
     'blog',
+    'users',
 ]
 
 MIDDLEWARE = [
@@ -135,3 +136,14 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+
+AUTH_USER_MODEL = 'users.User'
+LOGIN_REDIRECT_URL = '/'
+LOGOUT_REDIRECT_URL = '/'
+
+EMAIL_HOST = 'smtp.yandex.ru'
+EMAIL_PORT = 465
+EMAIL_HOST_USER = 'fuckup@oscarbot.ru'
+EMAIL_HOST_PASSWORD = 'AsTSNVv7pun9'
+EMAIL_USE_SSL = True
--- config/urls.py
+++ config/urls.py
@@ -20,8 +20,9 @@ from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
-      path('admin/', admin.site.urls),
-      path('', include('catalog.urls', )),
-      path('blog/', include('blog.urls', namespace='blog'))
+                  path('admin/', admin.site.urls),
+                  path('', include('catalog.urls', )),
+                  path('blog/', include('blog.urls', namespace='blog')),
+                  path('users/', include('users.urls', namespace='users')),
 
-      ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+              ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
